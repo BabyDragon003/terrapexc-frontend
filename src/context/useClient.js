9ac657577044fcee839aa661068968a9857171b6
BabyDragon003 <golddragon01234@gmail.com>
2020-05-06 14:11:32 +0900

    "2020-05-06 14:11"

--- src/context/useClient.js
+++ src/context/useClient.js
@@ -3,16 +3,11 @@ import { createContext, useContext, useMemo } from 'react';
 import { networks, NET_NAME } from "../utils/networks";
 
 function buildClient(network) {
-
-const NetworkContext = createContext();
-const useClient = () => useContext(NetworkContext);
-
-const NetworkProvider = ({ children }) => {
-  const network = networks[NET_NAME];
-  const terraClient = useMemo(() => {
-    if (network) {
-      return buildClient(network);
-    }
+  return new LCDClient({
+    URL: network.lcd,
+    chainID: network.chainID,
+    gasPrices: network.gasPrices
+  });
   }, [network]);
 
   const value = { terraClient, network };
--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -3,10 +3,8 @@ import { Link } from '@reach/router';
 
 export default function notfound() {
   return (
-            Back to Home <i className="icon ion-md-home"></i>
-          </Link>
-        </div>
-      </div>
-    </>
-  );
-}
+    <>
+      <div className="error-page vh-100 d-flex justify-content-center text-center">
+        <div className="my-auto">
+          <h2>404</h2>
+          <p>Oops something went wrong</p>
