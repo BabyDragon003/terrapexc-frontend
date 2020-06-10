5d65c614991d19e734ea3ceb8b99383a9fb3cecb
BabyDragon003 <golddragon01234@gmail.com>
2020-06-10 14:11:32 +0900

    "2020-06-10 14:11"

--- src/context/WalletConnectProvider.js
+++ src/context/WalletConnectProvider.js
@@ -3,6 +3,12 @@ import {
   getChainOptions,
 } from "@terra-money/wallet-provider"
 import { useEffect, useState } from "react"
+import WalletModal, { useModal } from "../components/WalletModal";
+import { ConnectModalProvider } from "./useConnectModal";
+import { NetworkProvider } from "./useClient"
+import { ContractProvider } from "./useContract"
+
+const WalletConnectProvider = ({
   children,
 }) => {
   const modal = useModal()
